8405c20a70c9d0cb195ef0d9e91bed2d7bd1b02d
Antonio Gagliardo <ajgagliardo92@gmail.com>
2025-03-15 23:23:03 -0700

    made a file for my progress on the animation of the spritesheet that is correct

--- prev/07 spriteSheetAnimation.js
+++ prev/07 spriteSheetAnimation.js
@@ -28,57 +28,50 @@ import { initDevtools } from "@pixi/devtools";
   const atlasData = {
     frames: {
       talk1: {
-        frame: {
-          x: 0,
-          y: 0,
-          w: 350,
-          h: 350,
-        },
-        //coordinations to get the sprice
-        sourceSize: { w: 350, h: 350 }, //the actual sprite, coordinate on the sprite sheet
-
-        spritesSourceSize: { x: 0, y: 0, w: 350, h: 350 }, //space arund the sprite
-        // this can be done better with an application instead of manually
-        // an app can help me with the json to be more specific
+        // //w + h width adn size of image (should be the same in all )
+        frame: { x: 0, y: 0, w: 200, h: 200 }, // x and y are coordinates to find sprite
+        sourceSize: { w: 350, h: 350 },
+        spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       talk2: {
-        frame: { x: 350, y: 0, w: 350, h: 350 },
+        frame: { x: 200, y: 0, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       talk3: {
-        frame: { x: 700, y: 0, w: 350, h: 350 },
+        frame: { x: 400, y: 0, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       talk4: {
-        frame: { x: 1050, y: 0, w: 350, h: 350 },
+        frame: { x: 600, y: 0, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       talk5: {
-        frame: { x: 1400, y: 0, w: 350, h: 350 },
+        frame: { x: 800, y: 0, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
 
       walk1: {
-        frame: { x: 0, y: 350, w: 350, h: 350 },
+        //w and h shuold be the same in the images from the same animation
+        frame: { x: 0, y: 200, w: 200, h: 200 }, // for this I had to move Y , Y stays the same in next ones, x changes
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       walk2: {
-        frame: { x: 350, y: 350, w: 350, h: 350 },
+        frame: { x: 200, y: 200, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       walk3: {
-        frame: { x: 700, y: 350, w: 350, h: 350 },
+        frame: { x: 400, y: 200, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
       walk4: {
-        frame: { x: 1050, y: 350, w: 350, h: 350 },
+        frame: { x: 600, y: 200, w: 200, h: 200 },
         sourceSize: { w: 350, h: 350 },
         spriteSourceSize: { x: 0, y: 0, w: 350, h: 350 },
       },
@@ -96,9 +89,9 @@ import { initDevtools } from "@pixi/devtools";
   const spritesheet = new Spritesheet(texture, atlasData);
   await spritesheet.parse();
 
-  const animatedSprite = new AnimatedSprite(spritesheet.animations.talk);
+  const animatedSprite = new AnimatedSprite(spritesheet.animations.walk);
   app.stage.addChild(animatedSprite);
 
   animatedSprite.play();
-  animatedSprite.animationSpeed = 0.05;
+  animatedSprite.animationSpeed = 0.11;
 })();
